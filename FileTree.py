0e7fb285068be803101d78c9b7a3fad8a97df7ca
Athul Prakash <athulpa@gmail.com>
2023-04-12 13:45:07 +0530

    Major Refactor of existing features
    - 	Cleaned up meaningless names
    -	More regular structure of code/functions

--- FileTree.py
+++ FileTree.py
@@ -1,15 +1,7 @@
 
 import os
 
-def getFileTree(rootPath="."):
-    return {i:(getFileTree(os.path.join(rootPath,i)) if os.path.isdir(os.path.join(rootPath,i)) else {}) for i in os.listdir(rootPath)}
 
-def printFileTree(dt, ilvl = 0):
-    for i in dt.keys():
-        print('\t'*ilvl, i)
-        if(dt[i] is not None):
-            printFileTree(dt[i], ilvl+1)
-        
 class FileTree:
     def __init__(self, arg=None, **kwargs):
         if(arg is None):        
@@ -43,6 +35,105 @@ class FileTree:
     def isDir(self,key):
         return not(self.isFile(key))
     
+    def isEmpty(self):
+        return not bool(self._map)
+    
+    
+    
+    ######################
+    ## PRINTING METHODS
+    ######################
+    
+    # Only top-level items
+    def displayMethod0(self, preString='', separator='\n', endString='', folderMarker='', fileMarker=''):
+        ret = preString
+        for k in self._map.keys():
+            ret += separator
+            ret += (folderMarker) if (self.isDir(k)) else (fileMarker)
+            ret += str(k)
+        return ret
+    
+    # Show full tree with custom options
+    def displayMethod1(self, indentLvl=0, indentString='\t'):
+        retString = ''
+        for i in self._map.keys():
+            retString += indentString*indentLvl + str(i) + '\n'
+            if(self._map[i] is not None):
+                retString += self._map[i].displayMethod1(indentLvl+1,indentString)    
+        return retString
+
+    # Show full tree with custom options
+    def displayMethod2(self, preIndent='', indentString=' '*3, lineCharIdx=0, lineChars=['\\','/']):
+        retString = ''
+        for i in self._map.keys():
+            retString += preIndent + str(i) + '\n'
+            if(self._map[i] is not None):
+                nextIndent = preIndent + indentString + lineChars[lineCharIdx] + indentString
+                lineCharIdx = (lineCharIdx + 1) % len(lineChars)
+                retString += self._map[i].displayMethod2(nextIndent, indentString, 0, lineChars)
+        return retString
+    
+    # Common interface to call a select printing function
+    def show(self, mode=None, **kwargs):
+        ret = 'Not Implemented'
+        if(mode==2):
+            ret = self.displayMethod2(**kwargs)
+        elif(mode==1):
+            ret = self.displayMethod1(**kwargs)
+        elif(mode==0):
+            ret = self.displayMethod0(**kwargs)
+        print(ret)
+    
+    
+    
+    ########################
+    #  SET DIFFERENCE ALGO
+    ########################
+    
+    def setDifference(self,other):
+        if(type(other) != type(self)):
+            raise NotImplementedError()
+
+        d = {}
+        for i in self.keys():
+            if not(i in other.keys()):
+                d[i] = self._map[i]
+            else:
+                if(self.isDir(i)):
+
+                    if(other.isFile(i)):
+                        d[i] = self._map[i]
+                    else:
+                        diff = self._map[i].setDifference(other[i])
+                        if(diff):
+                            d[i] = diff
+                else:
+                    if(other.isDir(i)):
+                        d[i] = self._map[i]                   
+
+        return FileTree(d, argCheck=False)
+    
+
+    ####################
+    #  EQUALITY ALGOS
+    ####################
+    
+    # Shallow Equality
+    def isEqual_Shallow(self, other):
+        if(len(self) != len(other)):
+            return False
+        for i in self.keys():
+            if(i not in other.keys() or self.isDir(i)!=other.isDir(i)):
+                return False
+            
+    # Deep Equality
+    def isEqual_Deep(self, other):
+        return (self._map == other._map)
+    
+    ########################
+    #  PYTHONIC INTERFACE
+    ########################
+    
     def keys(self):
         return list(self._map.keys())
     
@@ -62,69 +153,35 @@ class FileTree:
     def __eq__(self, other):
         if(type(other)!=type(self)):
             return False
-        return (self._map == other._map)
+        return self.isEqual_Deep(other)
     
     def __bool__(self):
-        return bool(self._map)
-    
-    ### Printing Functions
-    def toString(self, indentLvl=0, indentString='\t'):
-        retString = ''
-        for i in self._map.keys():
-            retString += indentString*indentLvl + str(i) + '\n'
-            if(self._map[i] is not None):
-                retString += self._map[i].toString(indentLvl+1,indentString)    
-        return retString
-
-    def toString2(self, preIndent='', indentString=' '*3, lineCharIdx=0, lineChars=['\\','/']):
-        retString = ''
-        for i in self._map.keys():
-            retString += preIndent + str(i) + '\n'
-            if(self._map[i] is not None):
-                nextIndent = preIndent + indentString + lineChars[lineCharIdx] + indentString
-                lineCharIdx = (lineCharIdx + 1) % len(lineChars)
-                retString += self._map[i].toString2(nextIndent, indentString, 0, lineChars)
-        return retString
-    
-    def show(self, mode=None, **kwargs):
-        ret = 'Not Implemented'
-        if(mode==2):
-            ret = self.toString2(**kwargs)
-        else:
-            ret = self.toString(**kwargs)
-        print(ret)
-        
-    def __repr__(self):
-        return 'keys(' + str(list(self._map.keys())) + ')'
-    
-    def __str__(self):
-        return self.toString(0,'\t')
+        return not self.isEmpty()
     
     def __sub__(self,other):
-        if(type(other) != type(self)):
-            raise NotImplementedError()
+        return self.setDifference(other)
+    
+    def __str__(self):
+        return self.displayMethod1(0,'\t')
+    
+    def __repr__(self):
+        if(self.isEmpty()):
+            return "Nothing to see here!"
+        return self.displayMethod0(preString="TOP-LEVEL ITEMS :\n", folderMarker='(dir) ')
+    
         
-        d = {}
-        for i in self.keys():
-            #print("Checking", i)
-            #input()
-            if not(i in other.keys()):
-                d[i] = self._map[i]
-            else:
-                if(self.isDir(i)):
-                    
-                    if(other.isFile(i)):
-                        d[i] = self._map[i]
-                    else:
-                        #print("calling {0:} - {0:}".format(i))
-                        #input()
-                        diff = self._map[i] - other[i]
-                        if(diff):
-                            d[i] = diff
-                else:
-                    if(other.isDir(i)):
-                        d[i] = self._map[i]                   
-        
-        return FileTree(d, argCheck=False)
-        
-        
+
+
+####################
+#  OUTER FUNCTIONS
+####################
+
+def getFileTree(rootPath="."):
+    return {i:(getFileTree(os.path.join(rootPath,i)) if os.path.isdir(os.path.join(rootPath,i)) else {}) for i in os.listdir(rootPath)}
+
+def printFileTree(dt, ilvl = 0):
+    for i in dt.keys():
+        print('\t'*ilvl, i)
+        if(dt[i] is not None):
+            printFileTree(dt[i], ilvl+1)
+                
